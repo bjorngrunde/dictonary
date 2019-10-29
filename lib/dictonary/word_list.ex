e2e11ecbb3ba46682e35778f8ec78c8057d8dc50
Björn Grunde <bj6261gr@LUMAC1852.local>
2019-10-29 09:44:36 +0100

    changed structure

---
+++ lib/dictonary/word_list.ex
@@ -0,0 +1,13 @@
+defmodule Dictonary.WordList do
+  def start() do
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def random_word(words) do
+    words
+    |> Enum.random()
+  end
+end
